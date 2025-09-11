4b01175e63f6c14e32c0b812dfa61b9c8ce26939
Jesus <jesusdavidcordova11@gmail.com>
2025-09-11 00:06:14 -0500

    Update Proyecto.java

--- Proyecto.java
+++ Proyecto.java
@@ -13,3 +13,17 @@ public class Ordenaciones {
                       3. Ordenaci\u00f3n Interna - QuickSort
                       4. Ordenaci\u00f3n Externa (leer archivo .txt)
                       5. Salir""";
+        
+        int opcion;
+        do {
+            opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
+            switch (opcion) {
+                case 1 -> ejecutarBurbuja();
+                case 2 -> ejecutarInsercion();
+                case 3 -> ejecutarQuickSort();
+                case 4 -> ejecutarOrdenacionExterna();
+                case 5 -> JOptionPane.showMessageDialog(null, "Saliendo del programa...");
+                default -> JOptionPane.showMessageDialog(null, "Opción inválida");
+            }
+        } while (opcion != 5);
+    }
