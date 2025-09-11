2c5424c324d40da8797e564c8ade5815f2f59510
Jesus <jesusdavidcordova11@gmail.com>
2025-09-10 22:36:38 -0500

    Update Proyecto.java

--- Proyecto.java
+++ Proyecto.java
@@ -1,39 +1,15 @@
+import java.awt.HeadlessException;
 import javax.swing.*;
+import java.io.*;
+import java.util.*;
 
-public class Burbuja {
-    public static void main(String[] args) {
-        int[] numeros = {5, 3, 8, 4, 2};
-
-        JOptionPane.showMessageDialog(null, "Arreglo original: " + arrayToString(numeros));
-
-        burbuja(numeros);
-
-        JOptionPane.showMessageDialog(null, "Arreglo final ordenado: " + arrayToString(numeros));
-    }
-
-    public static void burbuja(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-
-                JOptionPane.showMessageDialog(
-                        null,
-                        "Intercambio entre posiciones " + j + " y " + (j + 1) + ":\n" + arrayToString(arr)
-                    );
-                }
-            }
-        }
-    }
-
-    public static String arrayToString(int[] arr) {
-        StringBuilder sb = new StringBuilder();
-        for (int n : arr) {
-            sb.append(n).append(" ");
-        }
-        return sb.toString();
-    }
-}
+public class Ordenaciones {
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        String menu = """
+                      Selecciona una opci\u00f3n:
+                      1. Ordenaci\u00f3n Interna - Burbuja
+                      2. Ordenaci\u00f3n Interna - Inserci\u00f3n
+                      3. Ordenaci\u00f3n Interna - QuickSort
+                      4. Ordenaci\u00f3n Externa (leer archivo .txt)
+                      5. Salir""";
